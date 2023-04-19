b96cf4c89842978c3dc710d2983bd064c69d8371
singizin <igsing96@gmail.com>
2023-04-20 01:11:52 +0700

    Крутая рамка аватара из Steam

--- front/main.js
+++ front/main.js
@@ -1,16 +1,24 @@
 import {makeLikeable} from "./funFunctions/like/index.js";
 import {createCounter} from "./funFunctions/like/likeCounter.js";
+import {placeBorder} from "./funFunctions/avatar/index.js";
 
 function makeFun(config) {
-    const {
-        portrait,
-    } = config
 
-    const portraitEl = document.querySelector(portrait)
+  const {
+    portrait,
+  } = config
 
+  const portraitEl = document.querySelector(portrait)
+
+  // portraitEl.src = new URL('./assets/2.png', import.meta.url).href
+  
+  placeBorder(portraitEl)
+  
+  window.onload = () => {
     const {increaseCount} = createCounter(portraitEl.parentElement)
 
     makeLikeable(portraitEl, increaseCount)
+  }
 }
 
 window.makeFun = makeFun
